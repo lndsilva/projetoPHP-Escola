07d10e129b6def6d2f250865fc2cc65b7aa55d2e
lndsilva <lndsilva@hotmail.com>
2024-10-30 09:30:15 -0300

    projeto cadastro de alunos e professores em arquivos php

--- gravar.php
+++ gravar.php
@@ -6,11 +6,41 @@ $nome = isset($_POST['nome']) ? $_POST['nome'] : null;
 $matricula = isset($_POST['matricula']) ? $_POST['matricula'] : null;
 
 if (!is_null($nome)) {
-    
+
     $filename = __DIR__ . DS . 'alunos.txt';
     require 'arquivo.php';
 
     //abre no começo para leitura
+    $handle = fopen($filename, 'r');
 
+    //inclusão
+    if (empty($matricula)) {
+        $ultimaMatricula = 0;
+        while (! feof($handle)) {
+            $record = explode(',', fread($handle, 80));
+            $ultimaMatricula = (isset($record[0]) && empty($record[0])) ? $ultimaMatricula : $record[0];
+        }
+        fclose($handle);
+        $handle = fopen(__DIR__ . DS . 'alunos.txt', 'a');
+        $matricula = $ultimaMatricula + 1;
+        fwrite($handle, substr($matricula . ',' . $nome . ',' . str_repeat(' ', 80), 0, 78) . ",\n", 80);
+        fclose($handle);
+    } else { //alteração
+        $tmpFilename = __DIR__ . DS . 'alunos.tmp';
+        $tmpHandle = fopen($tmpFilename, 'w');
+        while (! feof($handle)) {
+            $row = fread($handle,  80);
+            $record = explode(',', $row);
+            $ultimaMatricula = (isset($record[0]) && empty($record[0])) ? $ultimaMatricula : $record[0];
+            if ($ultimaMatricula == $matricula) {
+                fwrite($tmpHandle, substr($matricula . ',' . $nome . ',' . str_repeat(' ', 80), 0, 70) . ",\n", 80);
+            } else {
+                fwrite($tmpHandle, $row);
+            }
+        }
+        fclose($tmpHandle);
+        fclose($handle);
+        unlink($filename);
+        copy($tmpFilename, $filename);
+    }
 }
-
